37a0a9abe4853c345e00b59763d20b0c76f0440b
xalejandra0526 <143530873+xalejandra0526@users.noreply.github.com>
2023-09-05 09:56:31 -0600

    Add files via upload

---
+++ L1_xahg1259223.py
@@ -0,0 +1,15 @@
+"""print("Hola mundo")
+print("soy Ximena")
+
+print("Hola mundo", end=" ")
+print("soy Ximena", end="")
+
+La diferencia se encuentra que al usar "end" las palabras se unen y no se escrben en lineas diferentes
+texto = input()
+print(texto + " Hola")"""
+
+print("Ingrese su nombre")
+Nombre= input()
+print("Hola mundo")
+print("soy " + Nombre)
+
