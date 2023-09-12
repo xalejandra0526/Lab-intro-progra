bc036ac71de40c45b9b179e1058625e477354d20
xalejandra0526 <143530873+xalejandra0526@users.noreply.github.com>
2023-09-12 09:56:58 -0600

    Add files via upload

---
+++ Semana 6 Lab progra/L51XAHG1259223.py
@@ -0,0 +1,12 @@
+print("Ejercicio 1")
+x =input("Ingrese número entero: ")
+
+xnumero = int(x)
+
+if xnumero < 0:
+    print("negativo")
+elif xnumero > 0:
+    print("positivo")
+else:
+    print("igual a cero")
+
