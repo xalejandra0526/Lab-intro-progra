9ca497c8812df4ccdcc0efc0d619bf1e921cc0b7
xalejandra0526 <143530873+xalejandra0526@users.noreply.github.com>
2023-09-12 09:52:50 -0600

    Add files via upload

---
+++ L52XAHG1259223.py
@@ -0,0 +1,22 @@
+print("Ejercicio 2")
+x = input("Ingrese el número de día: ")
+
+xnumero = int(x)
+
+if xnumero == 1:
+    print("lunes")
+elif xnumero == 2:
+    print("martes")
+elif xnumero == 3: 
+    print("miercoles")
+elif xnumero == 4:
+    print("jueves")
+elif xnumero == 5:
+    print("viernes")
+elif xnumero == 6:
+    print("sábado")
+elif xnumero == 7:
+    print(domingo)
+else:
+    print("Error. El número ingresado debe ser entre 1 a 7")
+
